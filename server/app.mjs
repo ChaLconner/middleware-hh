b6611c508f824ea46c00ef60c16262c9ba8ceb32
ChaL <chaluntonvipusanapas@gmail.com>
2025-04-10 16:53:54 +0700

    fix: API Validation and Middleware

--- server/app.mjs
+++ server/app.mjs
@@ -1,11 +1,10 @@
 import express from "express";
-import { validateAssignment } from "./middleware/validateAssignment.mjs";
+import { validateAssignment } from "./middlewares/validateAssignment.mjs";
 
 const app = express();
 const port = 4001;
 
 app.use(express.json());
-
 
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
--- server/middlewares/validateAssignment.mjs
+++ server/middlewares/validateAssignment.mjs
@@ -1,30 +1,27 @@
-// // middleware/validateAssignment.mjs
-// export const assignmentValidationRules(req, res, next) = {
-//     if(!req.body.title) {
-//       return res.status(400).json({ message: "Request body is required" });
-//     }
-
-//     if(!req.body.content) {
-//       return res.status(400).json({ message: "Request body is required" });
-//     }
-
-//     if(!req.body.category) {
-//       return res.status(400).json({ message: "Request body is required" });
-//     }
-//     if(!req.body.email) {
-//         return res.status(400).json({ message: "Request body is required" });
-//         }
-// }
-  
-
-
 export const validateAssignment = (req, res, next) => {
-    const { title, content, category, email } = req.body;
-    if (!title || !content || !category || !email) {
-        return res.status(400).json({ message: "Request body is required" });
-    }
+  const { title, content, category, email } = req.body;
+  if (!title || !content || !category || !email) {
+    return res.status(400).json({ message: "Request body is required" });
+  }
 
-    next();
+  //The email of the assignment creator must be in a valid email format
+  const validateEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (!validateEmail.test(email)) {
+    return res.status(400).json({ message: "Email is invalid" });
+  }
+
+  //The category must be a string and one of "Math", "English", or "Biology" only
+  const categories = ["Math", "English", "Biology"];
+  if (!categories.includes(category)) {
+    return res.status(400).json({ message: "Category is invalid" });
+  }
+
+  //The content length must be between 500 and 1000 characters
+  if (content.length < 500 || content.length > 1000) {
+    return res.status(400).json({ message: "Content must be between 500 and 1000 characters" });
+  }
+
+  next();
 };
 
 
